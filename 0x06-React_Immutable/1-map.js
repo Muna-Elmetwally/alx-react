b241c9ebfde06ed6174e122ea230ff08f3862346
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-09-13 02:31:53 +0200

    .

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,16 +1,5 @@
 import { Map } from 'immutable';
 
-function getImmutableObject(obj) {
-  return Map(obj);
+export default function getImmutableObject(object) {
+  return Map(object);
 }
-
-// Example usage:
-const originalObject = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
-
-const immutableObject = getImmutableObject(originalObject);
-console.log(immutableObject);
