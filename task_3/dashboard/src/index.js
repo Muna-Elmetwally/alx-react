3abe76cab776ac99f17cbf22f15a0d7f15433719
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-10-02 07:17:15 +0200

    .

--- task_3/dashboard/src/index.js
+++ task_3/dashboard/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { uiReducer } from './reducers/uiReducers';
+import thunk from 'redux-thunk';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  uiReducer,
+  composeEnhancers(applyMiddleware(thunk))
+)
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
